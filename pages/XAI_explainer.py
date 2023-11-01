10ff258793b4b4b3fbafb3eb156e64af592e9838
Jonghyeon <128576949+pjh98110@users.noreply.github.com>
2023-11-02 02:05:25 +0900

    Update XAI_explainer.py

--- pages/XAI_explainer.py
+++ pages/XAI_explainer.py
@@ -6,18 +6,12 @@ import random
 import os
 import datetime as dt
 from streamlit_extras.switch_page_button import switch_page
-
-
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from explainerdashboard import ClassifierExplainer, ExplainerDashboard, RegressionExplainer
 from explainerdashboard.datasets import titanic_embarked, titanic_fare, feature_descriptions
 import dash_bootstrap_components as dbc
 import threading
 import graphviz
-
-
-
-
 
 if "page" not in st.session_state:
     st.session_state.page = "Survey"
@@ -30,7 +24,6 @@ SEED = 42
 def load_csv(path):
     return pd.read_csv(path)
 
-
 st.markdown(f"""
             <span style='font-size: 24px;'>
             <div style=" color: #000000;">
@@ -39,14 +32,12 @@ st.markdown(f"""
             </div>
             """, unsafe_allow_html=True)
 
-
 selected_xai = st.selectbox(
     label = "원하는 XAI_분석을 선택하세요.",
     options=["XAI_분류", "XAI_회귀"],
     placeholder="하나를 선택하세요.",
     help="선택한 XAI에 따라 다른 분석 결과를 제공합니다.",
     key="xai_key",)
-
 
 # st.markdown(f"""
 #             <span style='font-size: 14px;'>
@@ -65,11 +56,13 @@ selected_xai = st.selectbox(
 #             </div>
 #             """, unsafe_allow_html=True)
 
-
-
-
 if selected_xai == "XAI_분류":
     if st.button("XAI 분류 실행"):  # 버튼 생성
+
+        # 사용 가능한 포트 찾기
+        port1 = find_available_port(8055, 8100)
+        if port1 is None:
+            st.error("No available ports found!")
 
         # 함수로 대시보드를 실행
         def run_dashboard(port):
@@ -90,12 +83,12 @@ if selected_xai == "XAI_분류":
         dashboard = ExplainerDashboard(explainer, title="XAI Classification Dashboard", bootstrap=dbc.themes.MORPH)
 
         # 스레드를 사용하여 대시보드 실행
-        t = threading.Thread(target=lambda: run_dashboard(8055))
+        t = threading.Thread(target=lambda: run_dashboard(port1))
         t.start()
 
         st.components.v1.html(
-            """
-            <iframe src="http://localhost:8055" style="width:100%; height:600px;"></iframe>
+            f"""
+            <iframe src="http://localhost:{port1}" style="width:100%; height:600px;"></iframe>
             """,
             height=600,
         )
@@ -103,6 +96,11 @@ if selected_xai == "XAI_분류":
 
 elif selected_xai == "XAI_회귀":
     if st.button("XAI 회귀 실행"):
+
+        # 사용 가능한 포트 찾기
+        port2 = find_available_port(8060, 8100)
+        if port2 is None:
+            st.error("No available ports found!")
 
         # 회귀 대시보드
         X_train, y_train, X_test, y_test = titanic_fare()
@@ -121,13 +119,12 @@ elif selected_xai == "XAI_회귀":
             dashboard2.run(port=port)
 
         # 스레드를 사용하여 대시보드 실행
-        t = threading.Thread(target=lambda: run_dashboard2(8060))
+        t = threading.Thread(target=lambda: run_dashboard2(port2))
         t.start()
 
         st.components.v1.html(
-            """
-            <iframe src="http://localhost:8060" style="width:100%; height:600px;"></iframe>
+            f"""
+            <iframe src="http://localhost:{port2}" style="width:100%; height:600px;"></iframe>
             """,
             height=600,
         )
-
